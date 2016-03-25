1dc0c18e2bcf6142ce07fc940ebc758dae07586f
Paige-H <paige@paratu.net>
2016-03-25 14:44:37 +1300

    Added Search
    Test search box added, search page added.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var basex = require('basex');
+var client = new basex.Session("127.0.0.1", 1984, "admin", "admin");
+client.execute("OPEN Colenso");
+
+router.get('/search', function(req, res) {
+  var foundin = $(xml + ":contains(" + req.query.searchString + ")");
+  console.log(foundin);
+  res.render('search', { title: 'My Movie Store', content: req.query.searchString });
+
+});
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/",function(req,res){
+client.execute("XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0';" +
+" (//name[@type='place'])[1] ",
+function (error, result) {
+  if(error){ console.error(error);}
+  else {
+    res.render('index', { title: 'Colenso Database', place: result.result });
+  }
+    }
+    );
 });
+
 
 module.exports = router;
