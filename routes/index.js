39d0bb737f025f72ba8b893a9941976399b371b2
Paige-H <paige@paratu.net>
2016-03-26 10:56:52 +1300

    Fixes to XQUERY search
    Added search form on search page.
    Search supports XQUERIES with no declared namespace

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,27 @@ var client = new basex.Session("127.0.0.1", 1984, "admin", "admin");
 client.execute("OPEN Colenso");
 
 router.get('/search', function(req, res) {
-  var foundin = $(xml + ":contains(" + req.query.searchString + ")");
-  console.log(foundin);
-  res.render('search', { title: 'My Movie Store', content: req.query.searchString });
-
+  var searchTerm = req.query.searchString;
+  var stringArray = searchTerm.split(" ");
+  var completeQuery;
+  if(stringArray[0] === "XQUERY"){
+    var query = " ";
+    for(i=1;i<stringArray.length;i++){
+      query += stringArray[i]+ " ";
+    }
+    var completeQuery = stringArray[0]+ " declare default element namespace 'http://www.tei-c.org/ns/1.0'; " + query;
+  }
+  //client.execute("XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0'; " +
+//"//name[@type = 'place' and position() = 1 and . = '"+searchTerm+"']",
+  client.execute(completeQuery,
+  function (error, result) {
+    if(error){ console.error(error);}
+    else {
+      console.log("Result: " + result.result);
+      res.render('search', { title: 'Colenso Database', content: result.result });
+    }
+  }
+  );
 });
 
 /* GET home page. */
